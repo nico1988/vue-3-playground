33f96bb7e4d5676c2f83c39d2efa134109fb66e5
nico1988 <410827024@qq.com>
2022-08-30 13:12:19 +0800

    feat: add logo png

--- src/typescript/components/Header.vue
+++ src/typescript/components/Header.vue
@@ -1,32 +1,37 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { useVersion } from '../version';
-import { useStore } from 'vuex';
-import { State } from '../store';
+import { defineComponent } from "vue";
+import { useVersion } from "../version";
+import { useStore } from "vuex";
+import { State } from "../store";
 
 export default defineComponent({
-  name: 'Header',
+  name: "Header",
 
   setup() {
     const { state } = useStore<State>();
 
     return {
       version: useVersion(),
-      state
+      state,
     };
-  }
+  },
 });
 </script>
 
 <template>
-<div class="py-5 text-center">
-  <img class="d-block mx-auto mb-4" src="../../../logo.png" alt="" width="72" height="72">
-  <h2>Vue {{ version }} Playground</h2>
-  <p class="lead">
-    Below is an example form built with the Vue {{ version }} Composition API.
-  </p>
-</div>
-<Teleport to="#footer">
-  <p class="mb-1">&copy; {{ state.year }} blacksonic</p>
-</Teleport>
+  <div class="py-5 text-center">
+    <img
+      class="d-block mx-auto mb-4"
+      src="../../../logo.png"
+      alt=""
+      width="100"
+    />
+    <h2>Vue {{ version }} Playground</h2>
+    <p class="lead">
+      Below is an example form built with the Vue {{ version }} Composition API.
+    </p>
+  </div>
+  <Teleport to="#footer">
+    <p class="mb-1">&copy; {{ state.year }} blacksonic</p>
+  </Teleport>
 </template>
